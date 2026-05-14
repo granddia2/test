96706ab65cbc2bd6217e32e68f12d0cc0ea6e08e
peak diamond 2 <aedan.l.kennison@gmail.com>
2026-05-13 21:09:11 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,8 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import LiveChat from './components/LiveChat.tsx'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Live Chat />
   </StrictMode>,
 );
